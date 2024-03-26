ffff90580b34acb3df502388f3612e92bed301a6
HXHdevs <jordisonnicolas@gmail.com>
2024-03-26 00:02:05 -0600

    Writing annotations for each one query

--- surtimientos, rutas y programacion.sql
+++ surtimientos, rutas y programacion.sql
@@ -1,10 +1,11 @@
+-- 1 nos trae todos los registros 1 a 1 que si tienen relacion con la tabla surtimientos para comprobarlo ejecutar la query 2 cambiando el valor de fk_surtimiento =
 SELECT s.fechadiadeventa, s.pk_surtimiento, s.fk_ruta, d.*
 FROM tb_surtimiento s
 JOIN tb_surtimiento_dotaciones d 
 ON s.pk_surtimiento = d.fk_surtimiento
 ORDER BY s.fechadiadeventa, s.pk_surtimiento;
 
-
+-- 2 cambiar el valor solamente no trae 0 porque no existe en tabla suirtimientos y 1 porque no tiene asignados registros
 SELECT s.*, d.* 
 FROM tb_surtimiento s
 JOIN tb_surtimiento_dotaciones d 
